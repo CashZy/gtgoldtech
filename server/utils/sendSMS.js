c85def1d47c87d3a4cd1f1daf5890cbdfc4a9f9f
Awais Shahbaz <awaishahbaz@gmail.com>
2023-06-10 17:52:19 +0500

    work on otp

--- server/utils/sendSMS.js
+++ server/utils/sendSMS.js
@@ -13,10 +13,10 @@ const sendSMS = async(too, otp) => {
     const text = `Hello! Your OTP code is ${otp}.`
     console.log('to', too)
     console.log('otp', otp)
-    //:dev: not sending to original because of developer number
-    // await vonage.sms.send({to, from, text})
-    //     .then(resp => { console.log('Message sent successfully'); console.log(resp); })
-    //     .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
+    // :dev: not sending to original because of developer number
+    await vonage.sms.send({to, from, text})
+        .then(resp => { console.log('Message sent successfully'); console.log(resp); })
+        .catch(err => { console.log('There was an error sending the messages.'); console.error(err); });
 }
 
 export default sendSMS;
