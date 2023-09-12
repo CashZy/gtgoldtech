8669760c50d98c0b08ebf3168970c12df86094f7
CashZy <143998661+CashZy@users.noreply.github.com>
2023-09-12 13:52:27 +0530

    Update HomeHeader.vue

--- components/pages/home/HomeHeader.vue
+++ components/pages/home/HomeHeader.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="flex items-center justify-center" :style="{ height: '60px' }">
-    <h1 class="font-semibold text-white">Dole</h1>
+    <h1 class="font-semibold text-white">GT GOLD</h1>
   </div>
 </template>
